a29b10148f62070b6a0b65721b53f332fde75eae
Ishan <ishan1608@gmail.com>
2018-05-29 11:47:38 +0530

    Converting succesfully and passing on the logs to a log file by redirecting std_out and std_err

--- converter/utils.py
+++ converter/utils.py
@@ -8,6 +8,8 @@ class VideoConverter(object):
             inputs={'input.avi': None},
             outputs={'output.mp4': '-c:a aac -b:a 128k -c:v libx264 -crf 23 -vf scale=320:240'}
         )
-        ff.run()
-        print('conversion finished')
+        print(ff.cmd)
+        with open('conversion-log.txt', 'w+') as log_file:
+            ff.run(stdout=log_file, stderr=log_file)
+            print('conversion finished')
 
