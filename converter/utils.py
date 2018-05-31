89c4bde5c29fe620580f7afbde4f06cd11d230ce
Ishan <ishan1608@gmail.com>
2018-05-31 22:55:01 +0530

    Added DriveDownloader

--- converter/utils.py
+++ converter/utils.py
@@ -1,3 +1,6 @@
+from urllib.parse import urlparse
+
+import requests
 from ffmpy import FFmpeg
 
 
@@ -13,3 +16,39 @@ class VideoConverter(object):
             ff.run(stdout=log_file, stderr=log_file)
             print('conversion finished')
 
+
+class DriveDownloader(object):
+    # taken from this StackOverflow answer: https://stackoverflow.com/a/39225039
+
+    def download_shareable_link(self, shareable_link, output_file_name):
+        drive_file_id = urlparse(shareable_link).query.split('=')[-1]
+        self._download_file_from_google_drive(drive_file_id, output_file_name)
+
+    def _download_file_from_google_drive(self, id, destination):
+        url = "https://docs.google.com/uc?export=download"
+
+        session = requests.Session()
+
+        response = session.get(url, params={'id': id}, stream=True)
+        token = self.get_confirm_token(response)
+
+        if token:
+            params = {'id': id, 'confirm': token}
+            response = session.get(url, params=params, stream=True)
+
+        self.save_response_content(response, destination)
+
+    def get_confirm_token(self, response):
+        for key, value in response.cookies.items():
+            if key.startswith('download_warning'):
+                return value
+
+        return None
+
+    def save_response_content(self, response, destination):
+        CHUNK_SIZE = 32768
+
+        with open(destination, "wb") as f:
+            for chunk in response.iter_content(CHUNK_SIZE):
+                if chunk:  # filter out keep-alive new chunks
+                    f.write(chunk)
