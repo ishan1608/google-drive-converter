4543d940875cb05a373a11bc724146218e690ac9
Ishan <ishan1608@gmail.com>
2018-06-02 23:52:00 +0530

    Added to admin panel

--- converter/admin.py
+++ converter/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import DriveJob
 
 
-admin.site.register(DriveJob)
+class DriveJobAdmin(admin.ModelAdmin):
+    list_display = ('id', 'drive_shareable_link', 'result_link')
+    search_fields = ('drive_shareable_link', 'result_link')
+    readonly_fields = ('result_link', 'scheduled_job_id')
+
+
+admin.site.register(DriveJob, DriveJobAdmin)
