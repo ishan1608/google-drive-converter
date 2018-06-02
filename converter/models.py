1c7392e178af0956cca6be7ea1ed6913cc9ac98d
Ishan <ishan1608@gmail.com>
2018-06-03 01:28:57 +0530

    Cleaning up files and updating the status

--- converter/models.py
+++ converter/models.py
@@ -1,3 +1,5 @@
+import os
+
 from django.db.models import CharField, NullBooleanField
 from model_utils import Choices
 from model_utils.models import TimeStampedModel
@@ -33,14 +35,35 @@ class DriveJob(TimeStampedModel):
 
     def execute(self):
         print(u'Execute Job: {}, with quality: {} and link: {}'.format(self.id, self.quality, self.drive_shareable_link))
+        self.download_status = False
+        self.save()
         # Download File
         downloaded_file_path = u'downloaded/{}'.format(self.id)
         DriveDownloader().download_shareable_link(self.drive_shareable_link, downloaded_file_path)
+
+        self.download_status = True
+        self.conversion_status = False
+        self.save()
 
         # Convert File
         output_file_path = u'downloaded/output-{}.mp4'.format(self.id)
         log_path = u'downloaded/log-{}.log'.format(self.id)
         VideoConverter.convert(downloaded_file_path, output_file_path, log_path, self.quality)
 
+        self.conversion_status = True
+        self.upload_status = False
+        self.save()
+
+        # Delete downloaded file
+        os.remove(downloaded_file_path)
+
         # Upload File
-        S3Uploader.upload(output_file_path, 'converted-{}.mp4'.format(self.id))
+        converted_path = 'converted-{}.mp4'.format(self.id)
+        S3Uploader.upload(output_file_path, converted_path)
+
+        self.upload_status = True
+        self.result_link = u'https://s3.amazonaws.com/google-drive-converter/{}'.format(converted_path)
+        self.save()
+
+        # Delete Converted File
+        os.remove(output_file_path)
