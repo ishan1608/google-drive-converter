5a6431d7f51851a2267fc9a2ad6abbb8d5486266
Ishan <ishan1608@gmail.com>
2018-06-02 23:47:20 +0530

    Added a model to store the job data

--- converter/models.py
+++ converter/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from model_utils.models import TimeStampedModel
+from django.db.models import CharField, NullBooleanField
 
-# Create your models here.
+
+class DriveJob(TimeStampedModel):
+    scheduled_job_id = CharField(max_length=36, null=True)
+    drive_shareable_link = CharField(max_length=2048)
+    download_status = NullBooleanField()
+    conversion_status = NullBooleanField()
+    upload_status = NullBooleanField()
+    result_link = CharField(max_length=2048, null=True)
